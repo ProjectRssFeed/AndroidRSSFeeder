a46c266d9e20aa10787077829472847dd3774f80
Quentin <quentin.favre@epitech.eu>
2017-01-11 23:41:57 +0100

    Binding fail

--- RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/view/layout/MainActivity.java
+++ RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/view/layout/MainActivity.java
@@ -1,15 +1,18 @@
 package epitech.quentinfavre.rssfeeder.view.layout;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import epitech.quentinfavre.rssfeeder.R;
+import epitech.quentinfavre.rssfeeder.viewModel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        ActivityMainBinding bind =  DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        bind.setUser(new MainActivityViewModel());
     }
 }
--- RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/viewModel/MainActivityViewModel.java
+++ RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/viewModel/MainActivityViewModel.java
@@ -1,10 +1,13 @@
 package epitech.quentinfavre.rssfeeder.viewModel;
 
 import android.databinding.BaseObservable;
+import android.databinding.BindingBuildInfo;
 
 /**
  * Created by Quentin on 11/01/2017.
  */
 
 public class MainActivityViewModel extends BaseObservable {
+
+
 }
