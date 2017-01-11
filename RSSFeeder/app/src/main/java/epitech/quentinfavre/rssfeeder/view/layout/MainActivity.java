a4d22ff1672e612401aca55c091824667341f199
Quentin <quentin.favre@epitech.eu>
2017-01-11 22:48:52 +0100

    Start + LoginPage

---
+++ RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/view/layout/MainActivity.java
@@ -0,0 +1,15 @@
+package epitech.quentinfavre.rssfeeder.view.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import epitech.quentinfavre.rssfeeder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ RSSFeeder/app/src/main/java/epitech/quentinfavre/rssfeeder/viewModel/MainActivityViewModel.java
@@ -0,0 +1,10 @@
+package epitech.quentinfavre.rssfeeder.viewModel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by Quentin on 11/01/2017.
+ */
+
+public class MainActivityViewModel extends BaseObservable {
+}
