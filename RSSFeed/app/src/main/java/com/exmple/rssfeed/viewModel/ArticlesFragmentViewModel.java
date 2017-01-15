cb80f75a67faf5d8bc4ced297f36cf041a5dc8f4
Quentin <quentin.favre@epitech.eu>
2017-01-15 18:35:14 +0100

    Fragment ??????

---
+++ RSSFeed/app/src/main/java/com/exmple/rssfeed/viewModel/ArticlesFragmentViewModel.java
@@ -0,0 +1,11 @@
+package com.exmple.rssfeed.viewModel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by Quentin on 15/01/2017.
+ */
+
+public class ArticlesFragmentViewModel extends BaseObservable {
+
+}
---
+++ RSSFeed/app/src/test/java/com/exmple/rssfeed/viewModel/ArticleViewModelTest.java
@@ -0,0 +1,36 @@
+package com.exmple.rssfeed.viewModel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Quentin on 15/01/2017.
+ */
+public class ArticleViewModelTest {
+    @Test
+    public void getTitle() throws Exception {
+
+    }
+
+    @Test
+    public void getAuthor() throws Exception {
+
+    }
+
+    @Test
+    public void getText() throws Exception {
+
+    }
+
+    @Test
+    public void onClickArticle() throws Exception {
+
+    }
+
+    @Test
+    public void onClickAuthor() throws Exception {
+
+    }
+
+}
