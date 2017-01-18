b8539462af1407ccb72a1dc96283bab9ef0bf7ba
Quentin <quentin.favre@epitech.eu>
2017-01-18 21:17:53 +0100

    Unknow stage

--- RSSFeed/app/src/main/java/com/exmple/rssfeed/view/ArticleActivity.java
+++ RSSFeed/app/src/main/java/com/exmple/rssfeed/view/ArticleActivity.java
@@ -1,15 +1,44 @@
 package com.exmple.rssfeed.view;
 
+import android.databinding.DataBindingUtil;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
 
 import com.exmple.rssfeed.R;
+import com.exmple.rssfeed.databinding.ActivityArticleBinding;
+import com.exmple.rssfeed.viewModel.ArticleActivityViewModel;
 
-public class ArticleActivity extends AppCompatActivity {
+public class ArticleActivity extends AppCompatActivity  implements SwipeRefreshLayout.OnRefreshListener{
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mListPosts;
+    ProgressBar mProgressBar;
+
+    ArticleActivityViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article);
+        vm = new ArticleActivityViewModel();
+
+        ActivityArticleBinding bind = DataBindingUtil.setContentView(this, R.layout.activity_article);
+        bind.setViemodel(vm);
+        mProgressBar = (ProgressBar)findViewById(R.id.progress_indicator);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                vm.onRefresh();
+            }
+        });
+        mListPosts =(RecyclerView) findViewById(R.id.recycler_articles);
+    }
+
+    @Override
+    public void onRefresh() {
+        vm.onRefresh();
     }
 }
--- RSSFeed/app/src/main/java/com/exmple/rssfeed/view/fragment/ArticlesFragment.java
+++ RSSFeed/app/src/main/java/com/exmple/rssfeed/view/fragment/ArticlesFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.exmple.rssfeed.R;
-import com.exmple.rssfeed.viewModel.ArticlesFragmentViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
